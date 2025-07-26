7e27b1c072817ecfa2f84ea1c4cdf4d35aa9ddae
Casey <caseypascucci@gmail.com>
2025-07-26 13:53:17 -0700

    Move Capstone Here

---
+++ FilamentSpool.cpp
@@ -0,0 +1,72 @@
+#include <FilamentSpool.h>
+
+
+FilamentSpool::FilamentSpool() 
+    : id(""), material(""), color(""), diameterMM(1.75), 
+      totalGrams(0.0), remainingGrams(0.0), costPerKg(0.0) {}
+FilamentSpool::FilamentSpool(const std::string& id, const std::string& material, const std::string& color,
+                             double diameterMM, double totalGrams, double remainingGrams, double costPerKg)
+    : id(id), material(material), color(color), diameterMM(diameterMM),
+      totalGrams(totalGrams), remainingGrams(remainingGrams), costPerKg(costPerKg) {}
+FilamentSpool::FilamentSpool(const FilamentSpool& other)
+    : id(other.id), material(other.material), color(other.color), diameterMM(other.diameterMM),
+      totalGrams(other.totalGrams), remainingGrams(other.remainingGrams), costPerKg(other.costPerKg) {}
+FilamentSpool::~FilamentSpool() {}
+
+std::ostream& operator<<(std::ostream& os, const FilamentSpool& spool) {
+    os << "FilamentSpool [ID: " << spool.id 
+       << ", Material: " << spool.material 
+       << ", Color: " << spool.color 
+       << ", Diameter: " << spool.diameterMM 
+       << "mm, Total: " << spool.totalGrams 
+       << "g, Remaining: " << spool.remainingGrams 
+       << "g, Cost per Kg: $" << spool.costPerKg << "]";
+    return os;
+}
+bool FilamentSpool::operator==(const FilamentSpool& other) const {
+    return id == other.id && material == other.material && color == other.color &&
+           diameterMM == other.diameterMM && totalGrams == other.totalGrams &&
+           remainingGrams == other.remainingGrams && costPerKg == other.costPerKg;
+}
+double FilamentSpool::getDiameterMM() const {
+    return diameterMM;
+}
+double FilamentSpool::getTotalGrams() const {
+    return totalGrams;
+}
+double FilamentSpool::getRemainingGrams() const {
+    return remainingGrams;
+}
+double FilamentSpool::getCostPerKg() const {
+    return costPerKg;
+}
+std::string FilamentSpool::getId() const {
+    return id;
+}
+std::string FilamentSpool::getMaterial() const {
+    return material;
+}
+std::string FilamentSpool::getColor() const {
+    return color;
+}
+void FilamentSpool::setDiameterMM(double diameterMM) {
+    this->diameterMM = diameterMM;
+}
+void FilamentSpool::setTotalGrams(double totalGrams) {
+    this->totalGrams = totalGrams;
+}
+void FilamentSpool::setRemainingGrams(double remainingGrams) {
+    this->remainingGrams = remainingGrams;
+}
+void FilamentSpool::setCostPerKg(double costPerKg) {
+    this->costPerKg = costPerKg;
+}
+void FilamentSpool::setId(const std::string& id) {
+    this->id = id;
+}
+void FilamentSpool::setMaterial(const std::string& material) {
+    this->material = material;
+}
+void FilamentSpool::setColor(const std::string& color) {
+    this->color = color;
+}
---
+++ FilamentSpool.h
@@ -0,0 +1,43 @@
+#ifndef SPOOL_H
+#define SPOOL_H
+
+#include <iostream>
+#include <string>
+
+class FilamentSpool {
+    private:
+        std::string id;
+        std::string material;
+        std::string color;
+        double diameterMM;
+        double totalGrams;
+        double remainingGrams;
+        double costPerKg;
+    public:
+        FilamentSpool();
+        FilamentSpool(const std::string& id, const std::string& material, const std::string& color,
+                      double diameterMM, double totalGrams, double remainingGrams, double costPerKg);
+        FilamentSpool(const FilamentSpool& other);
+        ~FilamentSpool();
+
+        friend std::ostream& operator<<(std::ostream& os, const FilamentSpool& spool);
+        bool operator==(const FilamentSpool& other) const;
+
+        double getDiameterMM() const;
+        double getTotalGrams() const;
+        double getRemainingGrams() const;
+        double getCostPerKg() const;
+        std::string getId() const;
+        std::string getMaterial() const;
+        std::string getColor() const;
+
+        void setDiameterMM(double diameterMM);
+        void setTotalGrams(double totalGrams);
+        void setRemainingGrams(double remainingGrams);
+        void setCostPerKg(double costPerKg);
+        void setId(const std::string& id);
+        void setMaterial(const std::string& material);
+        void setColor(const std::string& color);
+};
+
+#endif
